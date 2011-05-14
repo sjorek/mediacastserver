ccd161e8c25a19931e75c73897205d3b2d1656be
Stephan Jorek <stephan.jorek@gmail.com>
2011-05-14 12:52:21 +0200

    real implementation

--- src/http_stream_server.py
+++ src/http_stream_server.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python
 
+import mimetypes
 import BaseHTTPServer
 import SimpleHTTPServer
 
 class StreamHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    pass
+    if not mimetypes.inited:
+        mimetypes.init() # try to read system mime.types
+    extensions_map = mimetypes.types_map.copy()
+    extensions_map.update({
+        '': 'application/octet-stream', # Default
+        '.m3u8': 'application/x-mpegURL',
+        '.ts': 'video/MP2T'
+        })
 
 if __name__ == "__main__":
     SimpleHTTPServer.test(HandlerClass=StreamHTTPRequestHandler,
