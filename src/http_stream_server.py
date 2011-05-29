8b3133a9c7d6c189d9effbc5ea8253ef706b0eb4
Stephan Jorek <stephan.jorek@gmail.com>
2011-05-29 18:12:23 +0200

    modified:   src/http_stream_server.py

--- src/http_stream_server.py
+++ src/http_stream_server.py
@@ -10,11 +10,20 @@ class StreamHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     extensions_map = mimetypes.types_map.copy()
     extensions_map.update({
         '': 'application/octet-stream', # Default
-        '.m3u8': 'application/x-mpegurl',
+        '.avi': 'video/x-msvideo',
+        '.flv': 'video/x-flv',
+        '.f4a': 'audio/mp4',
+        '.f4v': 'video/mp4',
         '.ts': 'video/mp2t',
-        '.ogv': 'video/ogg',
-        '.mp4': 'video/mp4',
+        '.m3u8': 'application/x-mpegurl',
+        '.m4a': 'audio/mp4',
         '.m4v': 'video/mp4',
+        '.mp4': 'video/mp4',
+        '.mov': 'video/quicktime',
+        '.qt': 'video/quicktime',
+        '.oga': 'audio/ogg',
+        '.ogg': 'audio/ogg',
+        '.ogv': 'video/ogg',
         '.webm': 'video/webm'
         })
 
